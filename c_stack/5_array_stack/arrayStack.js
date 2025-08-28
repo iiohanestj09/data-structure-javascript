b665695e0eb4bd30ebe2b4a96765f44a42a4a710
iiohanestj09 <putra.dae2005@gmail.com>
2025-08-28 12:27:09 +0700

    Update c.5 - Perbaiki beberapa method

--- c_stack/5_array_stack/arrayStack.js
+++ c_stack/5_array_stack/arrayStack.js
@@ -1,5 +1,6 @@
 /* Stack = struktur data dengan prinsip LIFO (Last In, First Out
-elemen Terakhir yg masuk adalah yg pertama keluar.  */
+elemen Terakhir yg masuk adalah yg pertama keluar.  
+=> arrayStack akan menyimpan Top nya berupa index items  */
 
 class Stack {
     constructor() {
@@ -13,14 +14,17 @@ class Stack {
 
     // Pop: hapus elemen paling atas
     pop() {
-        if (this.items.length === 0) console.log("Gagal pop, Stack kosong");
-        else this.items.pop();
+        if (this.items.length === 0) {
+            console.log("Gagal pop, Stack kosong");
+            return null;
+        }
+        else return this.items.pop();
     }
 
     // Peek: lihat elemen paling atas (top)
     peek() {
-        if (this.items.length === 0) console.log("Gagal peek, Stack kosong");
-        else console.log(`Top: ${this.items[this.items.length - 1]}`);
+        if (this.items.length === 0) return null;
+        else return this.items[this.items.length - 1];
     }
 
     // Size: mengembalikkan ukuran Stack
@@ -30,8 +34,7 @@ class Stack {
     
     // Display: tampilkan isi stack
     display() {
-        if (this.items.length === 0) console.log("Gagal dipslay, Stack kosong");
-        else console.log("[ " + this.items.slice().reverse().join(" -> ") + " ]")
+        console.log("[ " + this.items.slice().reverse().join(" -> ") + " ]")
         // .slice() -> bikin salinan array supaya .reverse() dan .join() tidak mengubah objek asli
         // .slice() -> sering dipakai buat console.log utk menampilkan operasi sementara tanpa mengubah objek asli
     }
