3431ab81050b2798aa24d7a7d3ee09a936a59c70
iiohanestj09 <putra.dae2005@gmail.com>
2025-08-28 16:28:47 +0700

    Update d.7 - Perbaiki testing

--- d_queue/7_array_queue/testing.js
+++ d_queue/7_array_queue/testing.js
@@ -3,7 +3,7 @@ const Queue = require("./arrayQueue");
 const que = new Queue();
 
 que.dequeue();
-console.log(que.front());
+console.log(que.peek());
 
 que.enqueue(10);
 que.enqueue(20);
@@ -12,7 +12,7 @@ que.enqueue(30);
 que.dequeue();
 que.enqueue(40);
 
-console.log(que.front());
+console.log(que.peek());
 console.log(que.items.length);
 
 que.display();
