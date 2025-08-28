0a5e6121adb42a7d179cf00bbd9cbdef327f3931
iiohanestj09 <putra.dae2005@gmail.com>
2025-08-28 12:31:49 +0700

    Update c.5 - Perbaiki output testing

--- c_stack/5_array_stack/testing.js
+++ c_stack/5_array_stack/testing.js
@@ -1,12 +1,17 @@
 const Stack = require("./arrayStack");  // import class
 let st = new Stack();
+
+st.pop();
+console.log(st.peek());
+
 st.push(10);
 st.push(20);
 st.push(30);
 
 st.pop();
 st.push(40);
+
+console.log(st.peek());
 console.log(st.size());
 
-st.peek();
 st.display();
