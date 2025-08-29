e9b0ce091227aedfe89b2114e4c9060fe29ae94c
iiohanestj09 <putra.dae2005@gmail.com>
2025-08-29 19:52:02 +0700

    Update g.15c - tambah removeEdge(v1, v2)

--- g_graph/15_adjMatrix_graph/15c_directed_unweighted/testing.js
+++ g_graph/15_adjMatrix_graph/15c_directed_unweighted/testing.js
@@ -14,11 +14,17 @@ gr.addEdge(3, 4);
 gr.addEdge(4, 0);
 gr.addEdge(4, 1);
 gr.addEdge(4, 3);
+
+gr.removeEdge(0, 1);
+gr.removeEdge(2, 1);
+gr.removeEdge(3, 0);
+gr.removeEdge(4, 3);
+
 gr.display();
 
 /* 0 1 2 3 4
-0| 0 1 1 0 0 
+0| 0 0 1 0 0 
 1| 0 0 1 0 0
-2| 0 1 0 1 1
-3| 1 0 1 0 1
-4| 1 1 0 1 0  */
+2| 0 0 0 1 1
+3| 0 0 1 0 1
+4| 1 1 0 0 0  */
